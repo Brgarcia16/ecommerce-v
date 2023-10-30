51a6257826eea5d6568145e4e81b6650f675dde3
Brayan Garcia <brayangarcia162000@gmail.com>
2023-10-30 17:41:38 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+
+import { NavBar } from './componets/NavBar';
+import { ItemListContainer } from './componets/itemListContainer';
+import './App.css'
+
+
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <ItemListContainer greeting="Primera Pre-Entrega"></ItemListContainer>
+    </>
+  );
+}
+
+
+
+export default App;
+
---
+++ src/componets/CartWidget.jsx
@@ -0,0 +1,7 @@
+import cart from '../assets/cart.png'
+
+export const CartWidget = () => {
+    return <><img src={cart} alt='carrito' width="25px"/>
+    <span>10</span>
+    </>;
+}
